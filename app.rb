9f66f6d9b2248684fa3b0e73045e0b509ea89cf6
Nick Shyrokovskiy <nshyrokovskiy@gmail.com>
2015-09-25 21:10:07 +0300

    remove all others document nil checks
    again this check should be done beforhand

--- app.rb
+++ app.rb
@@ -13,19 +13,14 @@ module TeachingsHelpers
 
   def archive_group_by_year(doc)
     doc.xpath('/archive/teachings').group_by do |teaching|
-      year = teaching.at_xpath('year')
-      next if year.nil?
-      year.content
+      teaching.at_xpath('year').content
     end
   end
 
   def theme_link(theme)
-      href = ""
-      page = theme.at_xpath('page')
-      if not page.nil?
-        file = "data/themes/#{page.content.strip}.xml"
-        href = File.exist?(file) ? "themes/#{page.content.strip}" : ""
-      end
+      page = theme.at_xpath('page').content.strip
+      file = "data/themes/#{page}.xml"
+      href = File.exist?(file) ? "themes/#{page}" : ""
       "<a href=\"#{href}\"> #{theme.at_xpath('title').content.strip} </a>"
   end
 end
