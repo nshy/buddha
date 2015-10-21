bed05e8a653a7ce94f9bfac3edea751a148a3b03
Nick Shyrokovskiy <nshyrokovskiy@gmail.com>
2015-10-21 20:19:54 +0300

    cleanup routines
    leave only
    1. reusable functions
    2. routines that could be run on a regular basis

--- routines.rb
+++ routines.rb
@@ -1,23 +1,9 @@
 require 'nokogiri'
 require_relative 'models'
-require 'sequel'
-require 'httpclient'
 require 'uri'
 
 def theme_file(name)
   "data/themes/#{name}.xml"
-end
-
-def themes_node_to_human
-  doc = nil
-  File.open('data/teachings.xml') do |file|
-    doc = ArchiveDocument.new(Nokogiri::XML(file))
-  end
-  doc.archive.teachings.each do |teachings|
-    teachings.theme.each do |theme|
-      File.rename(theme_file(theme.page), theme_file(theme.page2))
-    end
-  end
 end
 
 def parse_xml(path)
@@ -51,53 +37,6 @@ end
 def rename_elements(xml, xpath, name)
   xml.xpath(xpath).each do |e|
     e.name = name
-  end
-end
-
-def themes_delete_sizes
-  each_file('data/themes') do |path|
-    parse_xml(path) do |xml|
-      delete_elements(xml, '/theme/record/video_size')
-      delete_elements(xml, '/theme/record/audio_size')
-      save_xml(path, xml)
-    end
-  end
-end
-
-def themes_rename_tags_to_descriptions
-  each_file('data/themes') do |path|
-    parse_xml(path) do |xml|
-      rename_elements(xml, '/theme/record/tags', 'description')
-      save_xml(path, xml)
-    end
-  end
-end
-
-def check_url_size(url)
-  r = DB[:media_size].where(url: url)
-  return if r.count > 0
-  puts "checking #{url}"
-  size = HClient.head(URI.escape(url)).header['Content-Length'][0].to_i
-  if size == 0
-    puts 'FAILED'
-    return
-  end
-  DB[:media_size].insert(url: url, size: size)
-end
-
-DB = Sequel.connect('sqlite://buddha.db')
-HClient = HTTPClient.new
-
-def calc_sizes
-  each_file('data/themes') do |path|
-    parse_xml(path) do |xml|
-      xml.xpath('/theme/record/audio_url').each do |e|
-        check_url_size(e.text)
-      end
-      xml.xpath('/theme/record/video_url').each do |e|
-        check_url_size(e.text)
-      end
-    end
   end
 end
 
