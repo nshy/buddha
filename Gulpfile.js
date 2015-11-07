8c103a1c3f52d24b66f87b535f271631ab770c5c
Nick Shyrokovskiy <nshyrokovskiy@gmail.com>
2015-11-07 23:26:28 +0300

    use gulp to compile assets

---
+++ Gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  gulp.src('./assets/css/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('default', function () {
+  gulp.watch('./assets/css/*.scss', ['sass']);
+});
--- compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for path in assets/css/*.scss; do
-  file=`basename $path`
-  name=${file%.scss}
-  sass "$path" > "public/css/${name}.css"
-done
