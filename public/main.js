08d23fb1ad489c62d95c1cd3ba5eb1c81b67578b
Leonid Lukin <imitation.of@gmail.com>
2016-09-04 15:21:03 +0300

    layout: add slide menu for <=768px
    use bigSlide v0.12.0
    
    Signed-off-by: Nick Shirokovskiy <nshyrokovskiy@gmail.com>

--- public/main.js
+++ public/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
     $.scrollUp({
-      scrollTrigger: '<a id="scrollUp" href="#top"/>'
+      scrollTrigger: '<a id="scrollUp" href="#top" class="push"/>'
+    });
+    // order is significant as scroll up has to be pushed too
+    $('.menu-link').bigSlide({
+      side: 'right'
     });
 });
