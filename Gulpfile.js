7efd4a0968ad03d4f16b20c3376fd810de96e0bb
Nick Shirokovskiy <nshyrokovskiy@gmail.com>
2016-10-02 16:30:47 +0300

    gulp: fix to cleanup and make deps
    1. we need to clean dst directory
    2. then we need to make next dep chain:
     clean <- sass <- concat
    3. return! if you task does not return stream
     then dependency will not work

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,19 +3,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var clean = require('gulp-clean');
 
-gulp.task('sass', function () {
-  gulp.src('./assets/css/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'));
+gulp.task('clean', function () {
+  return gulp.src('public/css/*')
+          .pipe(clean({force: true}))
 });
 
-gulp.task('concat', function () {
-  gulp.src(['public/css/*.css', '!public/css/bundle.css'])
-    .pipe(concat('bundle.css'))
-    .pipe(gulp.dest('./public/css'));
+gulp.task('sass', ['clean'], function () {
+  return gulp.src('./assets/css/*.scss')
+          .pipe(sass().on('error', sass.logError))
+          .pipe(gulp.dest('./public/css'));
 });
+
+gulp.task('concat', ['sass'], function () {
+  return gulp.src(['public/css/*.css', '!public/css/bundle.css'])
+          .pipe(concat('bundle.css'))
+          .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('css', ['concat'])
 
 gulp.task('default', function () {
-  gulp.watch('./assets/css/*.scss', ['sass', 'concat']);
+  gulp.watch('./assets/css/*.scss', ['css']);
 });
