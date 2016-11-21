655c0b9f72112a3f89e53dbdb7435ec562599a6b
Nick Shirokovskiy <nshyrokovskiy@gmail.com>
2016-11-21 21:19:35 +0300

    records routines: add check script
    Script checks all audio and video url from teachings
    and print those that are not ok (http 200) or has wrong
    content type (because geshe.ru actually do not return 404 and
    instead serve page with 'OM MANI PADME HUM' text).

---
+++ routines/records/check.rb
@@ -0,0 +1,28 @@
+#!/bin/ruby
+
+require_relative '../../routines.rb'
+
+SiteData = '../../data'
+include TeachingsHelper
+
+def check_link(url, types)
+  return if url.nil?
+  e = URI.escape(url)
+  Net::HTTP.start(URI(e).hostname) do |http|
+    response = http.request_head(e)
+    if response.code != '200' or
+       not types.include?(response['content-type'])
+      puts url
+    end
+  end
+end
+
+teachings = load_teachings
+teachings.each do |id, season|
+  season.theme.each do |theme|
+    theme.record.each do |record|
+      check_link(record.audio_url, ['audio/mpeg'])
+      check_link(record.video_url, ['video/x-msvideo', 'video/mp4'])
+    end
+  end
+end
