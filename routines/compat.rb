9ac8daf3c5dd55190741a48f9ae728f15e477137
Nick Shirokovskiy <nshyrokovskiy@gmail.com>
2017-01-23 20:10:59 +0300

    routines: update compat.rb

--- routines/compat.rb
+++ routines/compat.rb
@@ -8,30 +8,29 @@ require_relative '../helpers'
 
 include CommonHelpers
 include NewsHelpers
+include TeachingsHelper
 
 # Generates compatibility links, so that old site links
 # are mapped to new site links like:
 # /content/?q=node/395 -> /news/2016-09-04/
 
+SiteData = '../data'
+
 # news map
-data_dir = "../data"
 list = []
-each_file("#{data_dir}/news") do |file|
-  id = File.basename(file).gsub(/.adoc$/, '')
-  news = NewsDocument.new(body_path("#{data_dir}/news/#{id}"))
-  list << { node: news.buddha_node.to_i, id: id }
+News.new("#{SiteData}/news").load.each do |p|
+  node = p[:news].buddha_node
+  next if node.nil?
+  list << { node: node.to_i, id: p[:slug] }
 end
 list.sort! { |a, b| a[:node] <=> b[:node] }
 list.each { |e| puts "/content/?q=node/#{e[:node]}: /news/#{e[:id]}/" }
 
 # teachings map
 list = []
-each_file("#{data_dir}/teachings") do |file|
-  File.open(file) do |file|
-    teachings = TeachingsDocument.new(Nokogiri::XML(file)).teachings
-    teachings.theme.each do |theme|
-      list << { node: theme.buddha_node.to_i, id: path_to_id(file) }
-    end
+load_teachings().each do |p|
+  p[:document].theme.each do |theme|
+    list << { node: theme.buddha_node.to_i, id: p[:id] }
   end
 end
 list.sort! { |a, b| a[:node] <=> b[:node] }
