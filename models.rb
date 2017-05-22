d1fd26071080c36b542a5242d9a52657d87a75df
Nick Shirokovskiy <nshyrokovskiy@gmail.com>
2017-05-22 19:05:44 +0300

    models: move week to utils class

--- models.rb
+++ models.rb
@@ -1,4 +1,5 @@
 require_relative 'xmldsl'
+require_relative 'utils'
 require 'date'
 
 class NewsDocument
@@ -112,62 +113,6 @@ class LibraryDocument < XDSL::Element
   elements :section do
     element :name, String, required: true
     elements :category
-  end
-end
-
-class Week
-  include Comparable
-
-  def initialize(date = Date.today)
-    @monday = date - (date.cwday - 1)
-  end
-
-  def monday
-    @monday
-  end
-
-  def sunday
-    @monday + 6
-  end
-
-  def day(cwday)
-    @monday + (cwday - 1)
-  end
-
-  def self.cwdays
-    1..7
-  end
-
-  def dates
-    monday..sunday
-  end
-
-  def prev
-    Week.new(@monday - 7)
-  end
-
-  def next
-    Week.new(@monday + 7)
-  end
-
-  def succ
-    self.next
-  end
-
-  def <=>(week)
-    self.monday <=> week.monday
-  end
-
-  def -(week)
-    (@monday - week.monday).numerator / 7
-  end
-
-  def +(num)
-    Week.new(@monday + 7 * num)
-  end
-
-  def to_s
-    "start at #{@monday}"
   end
 end
 
