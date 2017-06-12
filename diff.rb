6686afb1181a0f1d91f8f18529c54603ef7af609
Nick Shirokovskiy <nshyrokovskiy@gmail.com>
2017-06-12 12:11:16 +0300

    diff: support binary files

--- diff.rb
+++ diff.rb
@@ -1,9 +1,9 @@
-def diff_path(path, mode)
-  if mode == :changed
+def diff_path(path, action)
+  if action == :changed
     path
   else
     present = { added: 'Добавлен', deleted: 'Удален' }
-    "#{path} [#{present[mode]}]"
+    "#{path} [#{present[action]}]"
   end
 end
 
@@ -13,24 +13,31 @@ def diff_to_html(diff)
   l = lines.shift
   res = []
   while l and l.start_with?('diff')
-    mode = :changed
-    # skip till ---
-    l = lines.shift while not l.start_with?('---')
-    a = l.sub(/^--- /, '')
-    mode = :added if a == '/dev/null'
-    l = lines.shift
-    b = l.sub(/^\+\+\+ /, '')
-    mode = :deleted if b == '/dev/null'
+    action = :changed
+    mode = :text
+    l = lines.shift while not (l.start_with?('---') or l.start_with?('Binary files'))
+    if l.start_with?('---')
+      a = l.sub(/^--- /, '')
+      l = lines.shift
+      b = l.sub(/^\+\+\+ /, '')
+    else
+      m = /^Binary files (.+) and (.+) differ$/.match(l)
+      a = m[1]
+      b = m[2]
+      mode = :binary
+    end
+    action = :added if a == '/dev/null'
+    action = :deleted if b == '/dev/null'
     path = a != '/dev/null' ? a : b
     path = path.sub(/.\//, '')
     res << \
     <<-END
       <div class="file">
-        <div class="path">#{diff_path(path, mode)}</div>
+        <div class="path #{mode}">#{diff_path(path, action)}</div>
     END
     l = lines.shift
     while l and l[0] == '@'
-      if mode == :changed
+      if action == :changed
         lnum = /^@@ -([^,]+)/.match(l)[1]
         res << \
         <<-END
