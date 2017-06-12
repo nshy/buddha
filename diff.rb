57821d7c536f447400a706b3d601ecf00d9aea03
Nick Shirokovskiy <nshyrokovskiy@gmail.com>
2017-06-12 11:52:58 +0300

    diff: support deleted/added files

--- diff.rb
+++ diff.rb
@@ -1,27 +1,45 @@
+def diff_path(path, mode)
+  if mode == :changed
+    path
+  else
+    present = { added: 'Добавлен', deleted: 'Удален' }
+    "#{path} [#{present[mode]}]"
+  end
+end
+
 def diff_to_html(diff)
   klasses = { ' ' => 'context', '+' => 'add', '-' => 'del' }
   lines = diff.split("\n")
   l = lines.shift
   res = []
-  while l and l[0] = 'd'
-    # skip index, ---
-    lines.shift; lines.shift
-    path = lines.shift[6..-1]
+  while l and l.start_with?('diff')
+    mode = :changed
+    # skip till ---
+    l = lines.shift while not l.start_with?('---')
+    a = l.sub(/^--- /, '')
+    mode = :added if a == '/dev/null'
+    l = lines.shift
+    b = l.sub(/^\+\+\+ /, '')
+    mode = :deleted if b == '/dev/null'
+    path = a != '/dev/null' ? a : b
+    path = path.sub(/.\//, '')
     res << \
     <<-END
       <div class="file">
-        <div class="path">#{path}</div>
+        <div class="path">#{diff_path(path, mode)}</div>
     END
     l = lines.shift
     while l and l[0] == '@'
-      lnum = /^@@ -([^,]+)/.match(l)[1]
-      res << \
-      <<-END
-        <div class="line">
-          <span class="title">Строка:</span>
-          <span class="value">#{lnum}</span>
-        </div>
-      END
+      if mode == :changed
+        lnum = /^@@ -([^,]+)/.match(l)[1]
+        res << \
+        <<-END
+          <div class="line">
+            <span class="title">Строка:</span>
+            <span class="value">#{lnum}</span>
+          </div>
+        END
+      end
       l = lines.shift
       while l and klasses.keys.include?(c = l[0])
         res << "<pre class='#{klasses[c]}'>"
