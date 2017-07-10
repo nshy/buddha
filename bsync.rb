83e8675cdec95f507d9aa735b64760cd53a47a8d
Nick Shirokovskiy <nshyrokovskiy@gmail.com>
2017-07-10 20:51:49 +0300

    bsync: add commit function

--- bsync.rb
+++ bsync.rb
@@ -11,6 +11,7 @@ usage: bsync <command>
 Commands:
   init      init repository
   status    print repository status
+  commit    commit workset changes
 USAGE
 
 def usage
@@ -99,6 +100,57 @@ def status
   r.each { |i| puts "R #{i[0]} -> #{i[1]}" }
 end
 
+def path_steps(path)
+  s = path_split(path)
+  (1..s.size).to_a.map { |l| s.slice(0, l).join('/') }
+end
+
+def dirs_trace(files)
+  dirs = files.map { |p| File.dirname(p) }.uniq
+  # make sure we have all parent dirs too
+  dirs = dirs.map { |p| path_steps(p) }.flatten.uniq.sort
+end
+
+def prepare_dirs(files)
+  dirs_trace(files).each do |d|
+    Dir.mkdir(d) if not File.exists?(d)
+  end
+end
+
+def prepend_path(files, site)
+  files.map { |p| site.path(p) }
+end
+
+def dir_empty(path)
+  (Dir.entries(path) - [ '.', '..' ]).empty?
+end
+
+def cleanup_dirs(files)
+  dirs_trace(files).reverse.each do |d|
+    Dir.unlink(d) if Dir.exist?(d) and dir_empty(d)
+  end
+end
+
+def commit
+  check_initialized
+  update, add, delete = sync_diff
+
+  update.each do |p|
+    File.unlink(BASE.path(p))
+    File.link(WORK.path(p), BASE.path(p))
+  end
+
+  prepare_dirs(prepend_path(add, BASE))
+  add.each do |p|
+    w = WORK.path(p)
+    File.chmod(File.stat(w).mode & 0555, w)
+    File.link(w, BASE.path(p))
+  end
+
+  delete.each { |p| File.unlink(BASE.path(p)) }
+  cleanup_dirs(prepend_path(delete, BASE))
+end
+
 usage if ARGV.size < 1
 cmd = ARGV.shift
 case cmd
@@ -106,6 +158,8 @@ case cmd
     init
   when 'status'
     status
+  when 'commit'
+    commit
   else
     usage
 end
