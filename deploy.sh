81bb269bb9b4d2e5009c7a621e9d6b706e9419e3
Nick Shirokovskiy <nshyrokovskiy@gmail.com>
2017-09-30 17:12:55 +0300

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -xe
+
+# before stopping the server try to merge in upstream changes
+cd main
+git fetch server
+git rebase server/master
+cd ..
+
+ssh buddha.ru <<-END
+set -xe
+
+cd buddha
+sudo systemctl stop buddha.ru
+sudo systemctl stop buddha.ru-watch
+END
+
+git push
+cd main
+git push
+cd ..
+
+ssh buddha.ru <<-END
+set -xe
+
+cd buddha
+git pull
+
+cd main
+git pull
+cd ..
+
+sudo bundle install
+./create.rb
+./sync.rb
+rm -rf .cache/*
+sudo systemctl start buddha.ru
+sudo systemctl start buddha.ru-watch
+END
