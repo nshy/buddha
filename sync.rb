4145c82d11c837cfcf0c917c430a8499c5ac1242
Nick Shyrokovskiy <nshyrokovskiy@gmail.com>
2017-10-24 17:26:29 +0300

    sync: use sync.rb both to sync and watch

--- sync.rb
+++ sync.rb
@@ -1,14 +1,12 @@
 #!/usr/bin/ruby
 
+require 'listen'
 require_relative 'helpers'
 require_relative 'convert'
 require_relative 'assets'
 require_relative 'resources'
 
 include CommonHelpers
-
-$stdout.sync = true
-sync_lock
 
 module Sync
 
@@ -57,4 +55,61 @@ end
 
 end # module Sync
 
+module Watch
+
+def listen_to(dir, options = {})
+  l = Listen.to(dir.dir, relative: true) do |*d|
+    d = d.map { |s| s.select { |p| dir.match(p) } }
+    Cache.diffmsg(*d, 'a')
+    yield *d
+  end
+  l.start
+end
+
+def handle_resource(resource)
+  resource.dirs.each do |dir|
+    listen_to(dir) do |*d|
+      clean_errors(*d)
+      table_update(resource, *d)
+    end
+  end
+end
+
+def handle_assets(assets)
+  listen_to(assets.src) do |u, a, d|
+    mixin_changed = false
+    if assets.respond_to?(:mixins)
+      mixin_changed = u.delete(assets.mixins) != nil
+    else
+      clean_errors(u, a, d)
+    end
+    update_assets(assets, u, a, d, mixin_changed)
+  end
+end
+
+end # module Watch
+
+USAGE = <<USAGE
+usage: sync.rb [ --watch ]
+
+Options:
+  --watch                  continue watch for changes after sync
+
+USAGE
+
+$stdout.sync = true
+sync_lock
+
+watch = false
+while not ARGV.empty? and ARGV.first.start_with?('--')
+  case ARGV.shift
+    when '--watch' then watch = true
+    else usage
+  end
+end
+
 sync(Sync, true)
+if watch
+  sync(Watch, false)
+  sleep
+end
